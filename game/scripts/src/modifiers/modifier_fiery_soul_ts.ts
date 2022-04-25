8bb34cb835fffbd11675cdd75a614020a16181e5
kapaez <as13755019770@gmail.com>
2022-04-25 13:13:12 +0800

    新版赤魂

--- game/scripts/src/modifiers/modifier_fiery_soul_ts.ts
+++ game/scripts/src/modifiers/modifier_fiery_soul_ts.ts
@@ -10,6 +10,9 @@ export class modifier_fiery_soul_ts extends BaseModifier {
     IsHidden(): boolean {
         return this.GetStackCount() == 0
     }
+    DestroyOnExpire(): boolean {
+        return false
+    }
     OnCreated(params: object): void {
         SendToConsole('createFiery')
         this.max_stack = this.GetAbility().GetSpecialValueFor("max_stack")
@@ -20,17 +23,30 @@ export class modifier_fiery_soul_ts extends BaseModifier {
         ParticleManager.SetParticleControl(this.effect_id, 1, Vector(this.GetStackCount(), 0, 0))
     }
     OnIntervalThink(): void {
+        SendToConsole("inThink")
         this.StartIntervalThink(-1)
         this.SetStackCount(0)
         ParticleManager.SetParticleControl(this.effect_id, 1, Vector(this.GetStackCount(), 0, 0))
     }
-    OnAbilityExecuted(event: ModifierAbilityEvent): void {
-        if (event.target) {
-            SendToConsole(event.target.GetName())
-        } else {
-            SendToConsole(event.ability.GetName())
+    // OnAbilityExecuted(event: ModifierAbilityEvent): void {
+    //     if (event.target) {
+    //         SendToConsole(event.target.GetName())
+    //     } else {
+    //         SendToConsole(event.ability.GetName())
+    //     }
+    //     this.add_state(1)
+    // }
+    OnTakeDamage(event: ModifierInstanceEvent): void {
+        SendToConsole("onTakeDamage")
+        let attacker = event.attacker
+        let ability = event.inflictor
+        if (attacker == this.GetCaster()) {
+            SendToConsole("equalCaster")
+            if (ability.GetOwner()) {
+                SendToConsole("isAbility")
+                this.add_state(1)
+            }
         }
-        this.add_state(1)
     }
     add_state(state_num: number) {
         if (state_num > this.max_stack || state_num + this.GetStackCount() > this.max_stack) {
@@ -45,7 +61,7 @@ export class modifier_fiery_soul_ts extends BaseModifier {
         this.AddParticle(this.effect_id, false, false, -1, false, false)
     }
     DeclareFunctions(): ModifierFunction[] {
-        return [ModifierFunction.MOVESPEED_BONUS_PERCENTAGE, ModifierFunction.ATTACKSPEED_BONUS_CONSTANT, ModifierFunction.ON_ABILITY_EXECUTED]
+        return [ModifierFunction.MOVESPEED_BONUS_PERCENTAGE, ModifierFunction.ATTACKSPEED_BONUS_CONSTANT, ModifierFunction.ON_ABILITY_EXECUTED, ModifierFunction.ON_TAKEDAMAGE]
     }
     GetModifierMoveSpeedBonus_Percentage(): number {
         return this.GetStackCount() * this.ms_bonus
